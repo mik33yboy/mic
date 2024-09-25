aa4712aeb317e9aaec8624a980eb75adc67820b5
Michael <mikedayandante@gmail.com>
2024-09-25 22:32:04 +0800

    main

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Weather } from './weather/weather.component';
-
-export const routes: Routes = [
-  { path: '', component: Weather },
-  // Add other routes as needed
-];
--- src/app/weather/day.model.ts
+++
@@ -1,7 +0,0 @@
-interface Day {
-    name: string;
-    temperature: number;
-    image?: string;
-    date: string; // Add date property to the interface
-    condition: string; // Add condition property to the interface
-}
